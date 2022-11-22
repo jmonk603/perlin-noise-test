bdf776e01b6e0dbda816ab482f301577d7ba06de
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2022-11-22 10:57:12 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,32 @@
+var mySong, amplitude;
+
+function preload() {
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
+}
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    mySong.play();
+
+    amplitude = new p5.Amplitude();
 }
 
 function draw() {
-    background(220);
+    background(5);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    noFill();
+    stroke(255);
+
+    var level = amplitude.getLevel();
+
+    var vol = map(level, 0,1,0,10);
+
+    beginShape();
+    for (var x=0; x < width; x++){
+        var nx = map(x,0,width,0,vol);
+        var y = height * noise(nx);
+        vertex(x, y);
+    }
+    endShape();
 }
